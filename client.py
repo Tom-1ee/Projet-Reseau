540934692f5338d59da3fd5b8e45493e6301a116
Tom-1ee <tom.zinckpro@gmail.com>
2022-11-07 13:54:53 +0100

    one client can connect to server

--- client.py
+++ client.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
 
+from logging import exception
 import socket
 
 def main():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         hostname = socket.gethostname()
-        ip_address = socket.gethostbyname(hostname)
-    # LE PROBLEME ET LA, DANS LE CONNECT.
-        s.connect((ip_address, 7777))
+        s.connect(("localhost", 7777))
 
-        s.sendall("TEST")
+        s.sendall("TEST".encode())
         while True:
             data = s.recv(1024)
             if len(data) == 0:
                 break
             print("Received:", data)
         print("Connection closed.")
-    except:
-        print("ERROR")
+    except Exception as e:
+        print("CAUGHT IN EXCEPT : ", e)
 main()
