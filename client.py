8690050119f72c5b9efddb18fcfae258d0288246
Tom 1ee <tom.zinckpro@gmail.com>
2022-11-16 17:45:57 +0100

    tic tac toe completely functionnal. just not beautiful

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ from platform import java_ver
 from grid import *
 from logging import exception
 import socket
-
+import time
 def main():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,14 +13,13 @@ def main():
         while True:
             data = s.recv(1024)
             if len(data) == 0:
-                break
+                time.sleep(0.1)
             data = data.decode()
             if ("<class 'grid.grid'" in data):
-                print("AA")
                 #for i in range(3):
                     #print("|",symbols[i*3], "|",  symbols[i*3+1], "|",  symbols[i*3+2], "|");
-                data = repr(grid(data))
-                print(data.cells)
+                #data = repr(grid(data))
+                #print(data.cells)
                 print(data)
 
             elif("quelle case" in data):
@@ -29,11 +28,13 @@ def main():
             elif("Quel mode" in data):
                 mode = str(input(data))
                 s.sendall(mode.encode())
+            elif("END" in data):
+                break
             else:
                 print(data)
                 
            
         print("Connection closed.")
     except Exception as e:
-        print("Connection proble : ", e)
+        print("[ManMade Error] Disconnected because : ", e)
 main()
--- main.py
+++ main.py
@@ -4,18 +4,12 @@ from platform import java_ver
 from grid import *
 import  random
 from pickle import NONE
-import sys 
 import socket
 import select
 import time
-import os
-import threading
-
+import pdb
 
 def main():
-
-    Joueur1 = "noone"
-    Joueur2 = "noone"
     serv = socket.socket(socket.AF_INET6,socket.SOCK_STREAM)
     serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     serv.bind(("", 7777))
@@ -23,27 +17,19 @@ def main():
     clients = {serv:"serveur"}
     sockets,l1,l2 = [serv],[],[]
     nicknames = ["serv"]
+    #pdb.set_trace()
 
-
-################################################################################################################
-### TANT QUE len(nicknames) PAS SUPERIEUR A 3 OU QUE LE MODE CHOISIS EST PAS "SOLO" ON CONTINUE LA RECHERCHE ###
-################################################################################################################
     while True:
             reader, writer, error = select.select(sockets,l1,l2)
             for x in reader:
                 if x == serv:
                     sc, a = serv.accept()
                     nickname = str(sc.getpeername()) #gives out nickname
-                    if Joueur1 != "noone": 
-                        Joueur2 = nickname
-                    else:
-                        Joueur1 = nickname
                     clients[nickname] = sc #gives a name to the socket
                     print("client connected : ", nickname)
                     nicknames.append(nickname)
                     for other, socket_value in clients.items():
                         if other != nickname and socket_value != "serveur": #don't send message to self nor to server #SEND message to everyone about the new client who connected 
-                            print("aaaaaaaaaaaaaaa : " + str(socket_value) + " BBBBB " + str(type(socket_value)))
                             socket_value.send("client connected : ".encode() + nickname.encode() + " \n".encode())
                         else:
                             None
@@ -54,7 +40,7 @@ def main():
                         gamemode = "multiplayer"
                         grids = [grid(), grid(), grid()]
                         current_player = J1
-                        clients[nicknames[current_player]].sendall("Quel mode de jeu souhaitez vous jouez ? \n 1 pour solo \n n'importe quel autre charactère lancera la recherche multijoueur ".encode())
+                        clients[nicknames[current_player]].sendall("Quel mode de jeu souhaitez vous jouez ? \n 1 pour solo \n n'importe quel autre charactère lancera la recherche multijoueur \n".encode())
                         data = clients[nicknames[current_player]].recv(1500).decode()
                         try:
                             data = int(data)
@@ -79,42 +65,79 @@ def main():
                                     time.sleep(1)
                                     clients[nicknames[current_player]].sendall("Waiting For player•••".encode())
                                     time.sleep(1)
+
+                                    sc, a = serv.accept()
+                                    nickname = str(sc.getpeername()) #gives out nickname
+                                    clients[nickname] = sc #gives a name to the socket
+                                    print("client connected : ", nickname)
+                                    nicknames.append(nickname)
+                                    sc.sendall("you are connected, it's your opponent's turn \n ".encode())
+                                    for other, socket_value in clients.items():
+                                        if other != nickname and socket_value != "serveur": #don't send message to self nor to server #SEND message to everyone about the new client who connected 
+                                            socket_value.send("client connected : ".encode() + nickname.encode() + " \n".encode())
+                                        else:
+                                            None
                                 shot = -1
                                 while shot <0 or shot >=NB_CELLS:
-                                    clients[nicknames[current_player]].sendall(str(grids[current_player]).encode())
-                                    clients[nicknames[current_player]].sendall("quelle case allez-vous jouer ?".encode())
+                                    clients[nicknames[current_player]].sendall(str(grids[current_player]).encode() +  " \n ".encode())
+                                    time.sleep(0.05) #Without that, the third time in this loop sends both upper and lower line at the exact same time, making it look like only one sendall.
+                                    clients[nicknames[current_player]].sendall("quelle case allez-vous jouer ?".encode() +  " \n ".encode())
                                     data = "no"
                                     while (data == "no"):
-                                        print (data)
                                         time.sleep(0.5)
                                         data = clients[nicknames[current_player]].recv(1500).decode()
                                         try:
                                             data = int(data)
                                         except:
-                                            clients[nicknames[current_player]].sendall("Vous devez écrire un chiffre entre 0 et 8".encode())
-                                            break
+                                            clients[nicknames[current_player]].sendall("Vous devez écrire un chiffre entre 0 et 8, trompez vous, et se sera votre défaite".encode() +  " \n ".encode())
+                                            time.sleep(0.05) #Without that, the third time in this loop sends both upper and lower line at the exact same time, making it look like only one sendall.
+                                            clients[nicknames[current_player]].sendall("quelle case allez-vous jouer ? \n".encode())
+                                            data = clients[nicknames[current_player]].recv(1500).decode()
+                                            try:
+                                                data = int(data)
+                                            except:
+                                                clients[nicknames[current_player]].sendall("Dommage, vous perdez par forfait ! \n".encode())
+                                                clients[nicknames[current_player]].sendall("END".encode())
+                                                current_player = current_player%2+1
+                                                clients[nicknames[current_player]].sendall("Votre adversaire a abandonner ! \n".encode())
+                                                clients[nicknames[current_player]].sendall("END".encode())
+                                                break
                                         shot = data
-                                        data = "no"
                                     if (grids[0].cells[shot] != EMPTY):
                                         grids[current_player].cells[shot] = grids[0].cells[shot]
                                         grids[0].display()
                                     else:
                                         grids[current_player].cells[shot] = current_player
                                         grids[0].play(current_player, shot)
-                                        current_player = "current_player%2+1"
+                                        current_player = current_player%2+1
                                         grids[0].display()
+
                             else: #Jeu solo
+                                print("SOLO GAME")
                                 if current_player == J1:
                                     shot = -1
                                     while shot <0 or shot >=NB_CELLS:
-                                        clients[nicknames[current_player]].sendall(str(grids[current_player]).encode())
-                                        clients[nicknames[current_player]].sendall("quelle case allez-vous jouer ?".encode())
+                                        clients[nicknames[current_player]].sendall(str(grids[current_player]).encode() +  " \n ".encode())
+                                        time.sleep(0.05) #Without that, the third time in this loop sends both upper and lower line at the exact same time, making it look like only one sendall.
+                                        clients[nicknames[current_player]].sendall("quelle case allez-vous jouer ? \n".encode())
+
                                         data = clients[nicknames[current_player]].recv(1500).decode()
+                                        
                                         try:
                                             data = int(data)
                                         except:
-                                            clients[nicknames[current_player]].sendall("Vous devez écrire un chiffre entre 0 et 8".encode())
-                                            break
+                                            clients[nicknames[current_player]].sendall("Vous devez écrire un chiffre entre 0 et 8, trompez vous, et se sera votre défaite \n".encode())
+                                            time.sleep(0.05) #Without that, the third time in this loop sends both upper and lower line at the exact same time, making it look like only one sendall.
+                                            clients[nicknames[current_player]].sendall("quelle case allez-vous jouer ? \n".encode())
+                                            data = clients[nicknames[current_player]].recv(1500).decode()
+                                            try:
+                                                data = int(data)
+                                            except:
+                                                clients[nicknames[current_player]].sendall("Dommage, vous perdez par forfait ! \n".encode())
+                                                clients[nicknames[current_player]].sendall("END".encode())
+                                                break
+
+                                               
                                         shot = data
                                 else:
                                     shot = random.randint(0,8)
@@ -127,11 +150,35 @@ def main():
                                     grids[0].play(current_player, shot)
                                     current_player = current_player%2+1
                                 if current_player == J1:
-                                    grids[J1].display()
+                                    grids[0].display() #grid[J1]
                         print("game over")
                         grids[0].display()
                         if grids[0].gameOver() == J1:
-                            clients[nicknames[J1]].sendall("You Win !".encode())
+                            if (gamemode == "solo"):
+                                clients[nicknames[J1]].sendall("You Win !".encode())
+                                clients[nicknames[J1]].sendall(str(grids[0]).encode())
+                                clients[nicknames[J1]].sendall("END".encode())
+                            else:    
+                                clients[nicknames[J1]].sendall("You Win !".encode())
+                                clients[nicknames[J2]].sendall("You Loose !".encode())
+                                clients[nicknames[J2]].sendall(str(grids[0]).encode())
+                                clients[nicknames[J1]].sendall(str(grids[0]).encode())
+                                time.sleep(0.1)
+                                clients[nicknames[J2]].sendall("END".encode())
+                                clients[nicknames[J1]].sendall("END".encode())
                         else:
-                            clients[nicknames[J1]].sendall("You Loose !".encode())
+                            if (gamemode == "solo"):
+                                clients[nicknames[J1]].sendall("You Loose !".encode())
+                                clients[nicknames[J1]].sendall(str(grids[0]).encode())
+                                clients[nicknames[J1]].sendall("END".encode())
+                            else:
+                                clients[nicknames[J1]].sendall("You Loose !".encode())
+                                clients[nicknames[J2]].sendall("You Win !".encode())
+                                clients[nicknames[J2]].sendall(str(grids[0]).encode())
+                                clients[nicknames[J2]].sendall(str(grids[0]).encode())
+                                time.sleep(0.1)
+                                clients[nicknames[J2]].sendall("END".encode())
+                                clients[nicknames[J1]].sendall("END".encode())
+
+                        
 main()
